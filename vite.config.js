0590aa11dabb77c419d76544345ec6ef9cee01c6
Joel Cannon <joelcannon@mac.com>
2023-11-15 23:49:12 -0600

    add emptyOutDir: true,

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    emptyOutDir: true,
     // rollupOptions: {
     //   input: {
     //     main: resolve(__dirname, "src/index.html"),
