0b4580d49a80f83643d325bca05e5292bfa93982
Joel Cannon <joelcannon@mac.com>
2023-11-15 23:09:29 -0600

    fix outDir: "../dist",

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
+  build: {
+    outDir: "../dist",
+    // rollupOptions: {
+    //   input: {
+    //     main: resolve(__dirname, "src/index.html"),
+    //     map: resolve(__dirname, "src/map.html"),
+    //   },
+    // },
+  },
 });
